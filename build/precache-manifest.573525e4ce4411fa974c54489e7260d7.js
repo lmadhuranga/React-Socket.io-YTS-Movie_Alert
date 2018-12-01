b68893c7579f30114a19e6586e244e66c5f43ae8
lmadhuranga <lilan.maduranga@gmail.com>
2018-12-02 00:02:32 +0530

    make it better

---
+++ build/precache-manifest.573525e4ce4411fa974c54489e7260d7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "d438181a01f652a7d63e",
+    "url": "/static/js/main.d438181a.chunk.js"
+  },
+  {
+    "revision": "8eadf75cfbb14aa9f642",
+    "url": "/static/js/1.8eadf75c.chunk.js"
+  },
+  {
+    "revision": "d438181a01f652a7d63e",
+    "url": "/static/css/main.6a0a7b54.chunk.css"
+  },
+  {
+    "revision": "0645e837b21e6ddfcdbfd2de25434d14",
+    "url": "/index.html"
+  }
+];
